944b7ca0f52388c5e52926e93783c1173b696beb
Jeff <jcgilbert70@gmail.com>
2023-03-11 16:17:08 -0600

    working on schemas, index.js & typeDefs should be done, working on resolvers

---
+++ server/schemas/index.js
@@ -0,0 +1,6 @@
+// import typeDefs and resolvers
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+// export the typeDefs and resolvers
+module.exports = { typeDefs, resolvers };
