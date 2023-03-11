0e8bc372e108d2f8da66ff9ff39578a7a931862b
Jeff <jcgilbert70@gmail.com>
2023-03-11 16:27:37 -0600

    working on resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,24 +3,60 @@ const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-    Query:
-
-
-
-
-
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: userId })
+            }
+            throw new AuthenticationError("You are not logged in");
+        },
+    },
 
     Mutation: {
-        addUser:
+        addUser: async (parent, args) => {
+            // create user
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user };
+        },
 
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError("No user with this email");
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError("Incorrect password");
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
 
-        login:
+        saveBook: async (parent, { bookData }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $push: { savedBooks: bookData } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError("Please log in");
+        },
 
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId } } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError("Please log in");
+        },
+    },
+};
 
-
-        saveBook:
-
-
-
-
-        removeBook:
+module.exports = resolvers;
