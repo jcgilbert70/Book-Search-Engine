80bb334ede73c238ac7e4768322fc714c24a8d4b
Jeff <jcgilbert70@gmail.com>
2023-03-11 16:37:54 -0600

    updated server.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,10 +4,15 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
+
         me: async (parent, args, context) => {
-            if (context.user) {
-                return User.findOne({ _id: userId })
-            }
+          // if user context, find by id
+          if (context.user) {
+            const userData = await User.findOne({ _id: context.user._id }).select(
+              "password"
+            );
+            return userData;
+          }
             throw new AuthenticationError("You are not logged in");
         },
     },
