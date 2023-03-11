0fde2afb39e242b41d7acf87edb457bb41d2375b
Jeff <jcgilbert70@gmail.com>
2023-03-11 15:07:59 -0600

    added queries.js to client utils

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+// import gql from apollo
+import { gql } from "@apollo/client";
+
+// export - get me
+export const GET_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
